b557d4f95f4209f9cb0e89b3406ce622b80e940b
szumielxd <43210079+szumielxd@users.noreply.github.com>
2022-03-11 23:15:36 +0100

    Removed unneeded characters and replaced tabs with spaces

--- src/main/java/me/badbones69/crazyauctions/api/enums/GuiType.java
+++ src/main/java/me/badbones69/crazyauctions/api/enums/GuiType.java
@@ -1,48 +1,39 @@
 package me.badbones69.crazyauctions.api.enums;
 
 public enum GuiType {
-	
-	
-	BIDDING(27, "Settings.Bidding-On-Item"),
-	BUYING(9, "Settings.Buying-Item"),
-	EXPIRED_LIST(54, "Settings.Cancelled/Expired-Items", true),
-	CURRENT_LIST(54, "Settings.Players-Current-Items"),
-	CATEGORIES(54, "Settings.Categories"),
-	SHOP(54, "Settings.GUIName", true),
-	SEARCH(54, "Settings.Search", true),
-	;
-	
-	
-	private final int size;
-	private final String title;
-	private final boolean paginable;
-	
-	
-	private GuiType(int size, String title) {
-		this(size, title, false);
-	}
-	
-	
-	private GuiType(int size, String title, boolean paginable) {
-		this.size = size;
-		this.title = title;
-		this.paginable = paginable;
-	}
-	
-	
-	public int getSize() {
-		return this.size;
-	}
-	
-	
-	public String getTitlePath() {
-		return this.title;
-	}
-	
-	
-	public boolean isPaginable() {
-		return this.paginable;
-	}
-	
+    
+    BIDDING(27, "Settings.Bidding-On-Item"),
+    BUYING(9, "Settings.Buying-Item"),
+    EXPIRED_LIST(54, "Settings.Cancelled/Expired-Items", true),
+    CURRENT_LIST(54, "Settings.Players-Current-Items"),
+    CATEGORIES(54, "Settings.Categories"),
+    SHOP(54, "Settings.GUIName", true),
+    SEARCH(54, "Settings.Search", true);
+    
+    private final int size;
+    private final String title;
+    private final boolean paginable;
+    
+    private GuiType(int size, String title) {
+        this(size, title, false);
+    }
+    
+    private GuiType(int size, String title, boolean paginable) {
+        this.size = size;
+        this.title = title;
+        this.paginable = paginable;
+    }
+    
+    public int getSize() {
+        return this.size;
+    }
+    
+    public String getTitlePath() {
+        return this.title;
+    }
+    
+    public boolean isPaginable() {
+        return this.paginable;
+    }
 
 }
