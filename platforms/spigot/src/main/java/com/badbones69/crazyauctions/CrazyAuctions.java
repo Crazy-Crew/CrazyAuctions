16e172d0e0cd673e30ac4a4e77f37767521f891a
Ryder Belserion <no-reply@ryderbelserion.com>
2023-02-18 21:45:25 -0500

    It now is using the proper API

--- platforms/spigot/src/main/java/com/badbones69/crazyauctions/CrazyAuctions.java
+++ platforms/spigot/src/main/java/com/badbones69/crazyauctions/CrazyAuctions.java
@@ -3,18 +3,23 @@ package com.badbones69.crazyauctions;
 import com.badbones69.crazyauctions.api.economy.vault.VaultSupport;
 import com.badbones69.crazyauctions.api.enums.PluginSupport;
 import com.badbones69.crazyauctions.configs.Config;
-import net.dehya.ruby.PaperManager;
+import net.dehya.ruby.PaperRuby;
 import net.dehya.ruby.RubyCore;
+import net.dehya.ruby.command.RubyCommand;
+import net.dehya.ruby.player.RubyPlayerRegistry;
+import net.dehya.ruby.registry.senders.types.Console;
 import org.bukkit.plugin.java.JavaPlugin;
 import org.jetbrains.annotations.NotNull;
 import java.lang.reflect.Field;
 import java.nio.file.Path;
+import java.util.Collections;
+import java.util.List;
 
 public class CrazyAuctions extends JavaPlugin implements RubyCore {
 
     private static CrazyAuctions plugin;
 
-    private final PaperManager paperManager = new PaperManager(this, true);
+    private final PaperRuby paperRuby = new PaperRuby(this);
 
     private final PluginSupport pluginSupport;
 
@@ -58,16 +63,31 @@ public class CrazyAuctions extends JavaPlugin implements RubyCore {
     }
 
     @Override
-    public @NotNull Boolean isFileModuleActivated() {
-        return true;
+    public @NotNull Console getConsole() {
+        return this.paperRuby.getConsole();
+    }
+
+    @Override
+    public @NotNull RubyPlayerRegistry getPlayerRegistry() {
+        return null;
+    }
+
+    @Override
+    public @NotNull List<RubyCommand> getCommands() {
+        return Collections.emptyList();
+    }
+
+    @Override
+    public @NotNull String getPrefix() {
+        return Config.PREFIX;
     }
 
     public static CrazyAuctions getPlugin() {
         return plugin;
     }
 
-    public PaperManager getPaperManager() {
-        return this.paperManager;
+    public PaperRuby getPaperManager() {
+        return this.paperRuby;
     }
 
     public PluginSupport getPluginSupport() {
