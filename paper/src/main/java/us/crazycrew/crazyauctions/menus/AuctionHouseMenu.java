09d09c5d58ff9322ac4a67e3bac3520ab65e81d2
Ryder Belserion <no-reply@ryderbelserion.com>
2023-12-14 00:17:04 -0500

    Getters

--- paper/src/main/java/us/crazycrew/crazyauctions/menus/AuctionHouseMenu.java
+++ paper/src/main/java/us/crazycrew/crazyauctions/menus/AuctionHouseMenu.java
@@ -1,7 +1,10 @@
 package us.crazycrew.crazyauctions.menus;
 
 import com.ryderbelserion.cluster.api.builders.InventoryBuilder;
+import com.ryderbelserion.cluster.items.ItemBuilder;
+import com.ryderbelserion.cluster.items.ParentBuilder;
 import org.bukkit.entity.Player;
+import org.bukkit.inventory.Inventory;
 
 public class AuctionHouseMenu extends InventoryBuilder {
 
@@ -11,6 +14,12 @@ public class AuctionHouseMenu extends InventoryBuilder {
 
     @Override
     public InventoryBuilder build() {
+        // Get inventory.
+        Inventory inventory = getInventory();
+
+        // Create item builder.
+        ItemBuilder builder = ParentBuilder.of();
+
         return this;
     }
 }
