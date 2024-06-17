a21bfda27a3cd7ca1dbc229c64238380fd062940
Ryder Belserion <no-reply@ryderbelserion.com>
2024-06-16 21:44:36 -0400

    [build] set java version to 17

--- buildSrc/src/main/kotlin/java-plugin.gradle.kts
+++ buildSrc/src/main/kotlin/java-plugin.gradle.kts
@@ -26,14 +26,14 @@ repositories {
 
 java {
     toolchain {
-        languageVersion.set(JavaLanguageVersion.of(21))
+        languageVersion.set(JavaLanguageVersion.of(17))
     }
 }
 
 tasks {
     compileJava {
         options.encoding = Charsets.UTF_8.name()
-        options.release.set(21)
+        options.release.set(17)
     }
 
     javadoc {
