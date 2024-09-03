ae7203eee43268e60590b8dd5e927296b4543450
Ryder Belserion <no-reply@ryderbelserion.com>
2024-09-02 22:54:38 -0400

    update buildscript

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,13 +1,1 @@
 rootProject.name = "CrazyAuctions"
-
-pluginManagement {
-    repositories {
-        maven("https://repo.crazycrew.us/releases")
-
-        gradlePluginPortal()
-    }
-}
-
-plugins {
-    id("com.ryderbelserion.feather-settings") version "0.0.4"
-}
