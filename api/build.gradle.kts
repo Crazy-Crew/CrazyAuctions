4d0e8e8ceafcac7e46db68f7def22852404a0566
Ryder Belserion <contact@ryderbelserion.com>
2025-12-16 16:31:44 -0500

    Update api modules

--- api/build.gradle.kts
+++ api/build.gradle.kts
@@ -5,9 +5,10 @@ plugins {
     `paper-plugin`
 }
 
-project.group = "us.crazycrew.crazyauctions"
 project.description = "The official API for CrazyAuctions!"
-project.version = "0.1.0"
+project.group = "us.crazycrew.crazyauctions"
+
+val projectVersion = rootProject.property("api_version").toString()
 
 java {
     withSourcesJar()
@@ -19,16 +20,16 @@ tasks {
         val name = rootProject.name.replaceFirstChar { it.uppercase() }
         val options = options as StandardJavadocDocletOptions
 
-        options.encoding = Charsets.UTF_8.name()
-        options.overview("src/main/javadoc/overview.html")
-        options.use()
-        options.isDocFilesSubDirs = true
-        options.windowTitle("$name ${rootProject.version} API Documentation")
-        options.docTitle("<h1>$name ${rootProject.version} API</h1>")
         options.header = """<img src="https://raw.githubusercontent.com/Crazy-Crew/Branding/refs/heads/main/crazyauctions/png/64x64.png" style="height:100%">"""
-        options.bottom("Copyright © 2025 CrazyCrew")
-        options.linkSource(true)
+        options.windowTitle("$name $projectVersion API Documentation")
+        options.docTitle("<h1>$name $projectVersion API</h1>")
+        options.overview("src/main/javadoc/overview.html")
         options.addBooleanOption("html5", true)
+        options.bottom("Copyright © 2025 CrazyCrew")
+        options.encoding = Charsets.UTF_8.name()
+        options.linkSource(true)
+        options.isDocFilesSubDirs = true
+        options.use()
     }
 
     withType<com.jeff_media.fixjavadoc.FixJavadoc> {
@@ -51,9 +52,9 @@ publishing {
 
     publications {
         create<MavenPublication>("mavenJava") {
-            groupId = "${project.group}" // us.crazycrew.crazyauctions
+            groupId = "${project.group}"
             artifactId = project.name
-            version = "${project.version}"
+            version = projectVersion
 
             from(components["java"])
         }
