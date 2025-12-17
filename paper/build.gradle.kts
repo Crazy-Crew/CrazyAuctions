629977fbcc893b392349f0803fb7445ebce0b791
Ryder Belserion <contact@ryderbelserion.com>
2025-12-16 21:37:03 -0500

    Fix project groups

--- paper/build.gradle.kts
+++ paper/build.gradle.kts
@@ -2,7 +2,7 @@ plugins {
     `paper-plugin`
 }
 
-project.group = "${rootProject.group}.paper"
+project.group = "${rootProject.group}"
 
 repositories {
     maven("https://repo.nexomc.com/releases/")
